e57583f9123b14f6002473583917aa242ec50fcd
Maksym Moroz <maskmoroz@gmail.com>
2024-12-11 17:16:06 +0200

    added routes and environment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { FilmsListComponent } from './core/films-list/films-list.component';
+import { FilmComponent } from './core/film/film.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'films', component: FilmsListComponent },
+  { path: '', redirectTo: '/films', pathMatch: 'full' },
+  { path: 'films/:id', component: FilmComponent },
+];
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  apiKey: '264851de5e5db3779050acfdfb068bef',
+  language: 'uk-UA',
+};
